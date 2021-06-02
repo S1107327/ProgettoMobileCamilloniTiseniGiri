ad325fbc43810c86c95f2c5a46c873f072945bce
Andrea Camilloni <s1090325@studenti.univpm.it>
2021-06-02 18:05:21 +0200

    Login/registrati

--- app/src/main/java/com/example/progettomobilecamillonitisenigiri/MainActivity.kt
+++ app/src/main/java/com/example/progettomobilecamillonitisenigiri/MainActivity.kt
@@ -2,18 +2,28 @@ package com.example.progettomobilecamillonitisenigiri
 
 import android.app.SearchManager
 import android.content.Intent
+
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.example.progettomobilecamillonitisenigiri.databinding.ActivityMainBinding
-
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mAuth: FirebaseAuth
+    lateinit var mLogoutBtn: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         val binding: ActivityMainBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_main)
         val navHostFragment =
@@ -38,14 +48,39 @@ class MainActivity : AppCompatActivity() {
                 }
                 R.id.share -> {
                     val intent = Intent(Intent.ACTION_WEB_SEARCH)
-                    intent.putExtra(SearchManager.QUERY, "logout")
+                    intent.putExtra(SearchManager.QUERY, "share")
                     if (intent.resolveActivity(packageManager) != null) {
                         startActivity(intent)
                     }
                     true
                 }
-                else->false
+                R.id.logout -> {
+                    mAuth = FirebaseAuth.getInstance()
+                    FirebaseAuth.getInstance().signOut()
+                    val startIntent = Intent(applicationContext, LoginActivity::class.java)
+                    startActivity(startIntent)
+                    finish()
+                    true
+                }
+
+
+                else -> false
             }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        if (currentUser == null) {
+
+            val intent = Intent(applicationContext, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        } else {
+            Toast.makeText(applicationContext, "Login Successfully ", Toast.LENGTH_SHORT).show()
         }
     }
 }
