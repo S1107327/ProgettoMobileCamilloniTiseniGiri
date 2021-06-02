51eb81f7e8c6f354d67d8aee32ddc5d3416abb73
Matteo Giri <s1090263@studenti.univpm.it>
2021-06-02 11:26:38 +0200

    fragmentCategoria

---
+++ app/src/main/java/com/example/progettomobilecamillonitisenigiri/FragmentRicerca.kt
@@ -0,0 +1,27 @@
+package com.example.progettomobilecamillonitisenigiri
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class FragmentRicerca: Fragment(R.layout.fragment_ricerca) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val corso1:Corsi = Corsi()
+        val corso2:Corsi = Corsi()
+        val corso3:Corsi = Corsi()
+        val corso4:Corsi = Corsi()
+        val corso5:Corsi = Corsi()
+        val corso6:Corsi = Corsi()
+        val corso7:Corsi = Corsi()
+        val corso8:Corsi = Corsi()
+        val corso9:Corsi = Corsi()
+        val corso10:Corsi = Corsi()
+        val rvRicerca: RecyclerView = view.findViewById(R.id.recyclerViewRicerca)
+        rvRicerca.layoutManager = GridLayoutManager(context,2)
+        rvRicerca.adapter = PopularAdapter(mutableListOf<Corsi>(corso1,corso2,corso3,corso4,corso5,corso6,corso7,corso8,corso9,corso10))
+    }
+}
