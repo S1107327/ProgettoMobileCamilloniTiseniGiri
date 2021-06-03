37243218823ba356a5935169ced65071aef0257f
Andrea Camilloni <s1090325@studenti.univpm.it>
2021-06-03 15:04:08 +0200

    aggiunta campi registrazione + possibilità di modificare dati in area utente

--- app/src/main/java/com/example/progettomobilecamillonitisenigiri/FragmentUser.kt
+++ app/src/main/java/com/example/progettomobilecamillonitisenigiri/FragmentUser.kt
@@ -2,11 +2,78 @@ package com.example.progettomobilecamillonitisenigiri
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import android.widget.Toast.makeText
 import androidx.fragment.app.Fragment
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.*
+import com.google.firebase.ktx.Firebase
 
-class FragmentUser: Fragment(R.layout.fragment_user) {
+
+class FragmentUser : Fragment(R.layout.fragment_user) {
+    //Firebase references
+    private var mDatabaseReference: DatabaseReference? = null
+    private var mDatabase: FirebaseDatabase? = null
+    private var mAuth: FirebaseAuth? = null
+
+    //UI elements
+    private var tvFirstName: EditText? = null
+    private var tvLastName: EditText? = null
+    private var tvEmail: TextView? = null
+    lateinit var mSaveBtn: Button
+
+    private var mUser:FirebaseUser?=null
+
+    override fun onStart() {
+        super.onStart()
+
+        mUser = mAuth!!.currentUser
+        val mUserReference = mDatabaseReference!!.child(mUser!!.uid)
+
+        tvEmail!!.text = mUser!!.email
+
+
+        mUserReference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                tvFirstName!!.setText(snapshot.child("firstName").value as String)
+                tvLastName!!.setText(snapshot.child("lastName").value as String)
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {}
+        })
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initialise()
+
+
     }
+
+    private fun initialise() {
+
+        mDatabase = FirebaseDatabase.getInstance()
+        mDatabaseReference = mDatabase!!.reference!!.child("Users")
+        mAuth = FirebaseAuth.getInstance()
+        tvFirstName = getView()?.findViewById<View>(R.id.tv_first_name) as EditText
+        tvLastName = getView()?.findViewById<View>(R.id.tv_last_name) as EditText
+        tvEmail = getView()?.findViewById<View>(R.id.tv_email) as TextView
+        mSaveBtn = getView()?.findViewById(R.id.save_btn) as Button
+
+        mSaveBtn.setOnClickListener{
+            mUser?.let { it1 -> mDatabaseReference!!.child(it1.uid).child("firstName").setValue(
+                tvFirstName!!.text.toString()) }
+            mUser?.let { it1 -> mDatabaseReference!!.child(it1.uid).child("lastName").setValue(tvLastName!!.text.toString()) }
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/progettomobilecamillonitisenigiri/RegisterActivity.kt
+++ app/src/main/java/com/example/progettomobilecamillonitisenigiri/RegisterActivity.kt
@@ -4,36 +4,64 @@ import android.app.ProgressDialog
 import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class RegisterActivity : AppCompatActivity() {
 
-    lateinit var mRegisterBtn : Button
-    lateinit var mRegisterEmail : EditText
-    lateinit var mRegisterPassword : EditText
-    lateinit var mRegisterLoginBtn : Button
-    lateinit var mProgressbar : ProgressDialog
+    lateinit var mRegisterBtn: Button
+    lateinit var mRegisterEmail: EditText
+    lateinit var mRegisterPassword: EditText
+    lateinit var mRegisterLoginBtn: Button
+    lateinit var mProgressbar: ProgressDialog
 
+    private var FirstName: EditText? = null
+    private var LastName: EditText? = null
+
+    //Firebase references
+    private var mDatabaseReference: DatabaseReference? = null
+    private var mDatabase: FirebaseDatabase? = null
     lateinit var mAuth: FirebaseAuth
+
+    private val TAG = "CreateAccountActivity"
+
+    //global variables
+    private var firstName: String? = null
+    private var lastName: String? = null
+    private var email: String? = null
+    private var password: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        initialise()
 
-        mRegisterBtn = findViewById(R.id.RegisterBtn)
-        mRegisterLoginBtn = findViewById(R.id.RegisterLoginBtn)
+    }
 
-        mRegisterEmail = findViewById(R.id.RegisterEmail);
-        mRegisterPassword = findViewById(R.id.RegisterPassword)
+    private fun initialise() {
+        FirstName = findViewById<View>(R.id.first_name) as EditText
+        LastName = findViewById<View>(R.id.last_name) as EditText
+        mRegisterEmail = findViewById<View>(R.id.RegisterEmail) as EditText
+        mRegisterPassword = findViewById<View>(R.id.RegisterPassword) as EditText
 
-        mAuth = FirebaseAuth.getInstance();
+        mRegisterBtn = findViewById<View>(R.id.RegisterBtn) as Button
+
+        mProgressbar = ProgressDialog(this)
+        mDatabase = FirebaseDatabase.getInstance()
+        mDatabaseReference = mDatabase!!.reference!!.child("Users")
+        mAuth = FirebaseAuth.getInstance()
+
         mProgressbar = ProgressDialog(this)
 
+        mRegisterLoginBtn = findViewById(R.id.RegisterLoginBtn)
         mRegisterLoginBtn.setOnClickListener {
 
             val loginIntent = Intent(applicationContext, LoginActivity::class.java)
@@ -41,48 +69,95 @@ class RegisterActivity : AppCompatActivity() {
 
         }
 
+        mRegisterBtn!!.setOnClickListener { createNewAccount() }
 
-
-        mRegisterBtn.setOnClickListener {
-            val email = mRegisterEmail.text.toString().trim()
-            val password = mRegisterPassword.text.toString().trim()
-
-
-            if (TextUtils.isEmpty(email)) {
-
-                mRegisterEmail.error = "Enter Email"
-                return@setOnClickListener
-            }
-            if (TextUtils.isEmpty(password)) {
-
-                mRegisterPassword.error = "Enter Password"
-                return@setOnClickListener
-            }
-
-            createUser(email, password)
-        }
     }
 
-    fun createUser( email: String, password: String) {
+    private fun createNewAccount() {
 
-        mProgressbar.setMessage("Please wait..")
-        mProgressbar.show()
+        firstName = FirstName?.text.toString()
+        lastName = LastName?.text.toString()
+        email = mRegisterEmail?.text.toString().trim()
+        password = mRegisterPassword?.text.toString().trim()
+        //inserire qui i controlli sui campi
+        if (TextUtils.isEmpty(firstName)||TextUtils.isEmpty(lastName)) {
+            Toast.makeText(this, "Enter all details", Toast.LENGTH_SHORT).show()
+            return
+        }
 
+        if (TextUtils.isEmpty(email)) {
+            mRegisterEmail.error = " Enter Email"
+            return
+        }
+        if (TextUtils.isEmpty(password)) {
+            mRegisterPassword.error = " Enter Password"
+            return
+        }
 
-        mAuth.createUserWithEmailAndPassword(email, password)
+        if (!TextUtils.isEmpty(firstName) && !TextUtils.isEmpty(lastName)
+            && !TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)
+        ) {
+        } else {
+            Toast.makeText(this, "Enter all details", Toast.LENGTH_SHORT).show()
+        }
+        mProgressbar!!.setMessage("Registering User...")
+        mProgressbar!!.show()
+        mAuth!!
+            .createUserWithEmailAndPassword(email!!, password!!)
+            .addOnCompleteListener(this) { task ->
+                mProgressbar!!.hide()
+
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+
+                    val userId = mAuth!!.currentUser!!.uid
+
+                    //Verify Email
+                    verifyEmail();
+
+                    //update user profile information
+                    val currentUserDb = mDatabaseReference!!.child(userId)
+                    currentUserDb.child("firstName").setValue(firstName)
+                    currentUserDb.child("lastName").setValue(lastName)
+
+                    updateUserInfoAndUI()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        this@RegisterActivity, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
+
+    private fun updateUserInfoAndUI() {
+
+        //start next activity
+        val intent = Intent(this@RegisterActivity, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        startActivity(intent)
+    }
+
+    private fun verifyEmail() {
+        val mUser = mAuth!!.currentUser;
+        mUser!!.sendEmailVerification()
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-
-                    val Intent = Intent(applicationContext, MainActivity::class.java)
-                    startActivity(Intent)
-                    finish()
-                    mProgressbar.dismiss()
-
+                    Toast.makeText(
+                        this@RegisterActivity,
+                        "Verification email sent to " + mUser.getEmail(),
+                        Toast.LENGTH_SHORT
+                    ).show()
                 } else {
-
-                    Toast.makeText(this, "Authentication failed.${task.exception}", Toast.LENGTH_SHORT).show()
-                    mProgressbar.dismiss()
-
+                    Log.e(TAG, "sendEmailVerification", task.exception)
+                    Toast.makeText(
+                        this@RegisterActivity,
+                        "Failed to send verification email.",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             }
     }
