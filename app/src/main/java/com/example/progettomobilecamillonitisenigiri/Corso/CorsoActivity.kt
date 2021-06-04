0971894a51950108a8d837e60438e0a1366790df
Matteo Giri <s1090263@studenti.univpm.it>
2021-06-04 18:53:33 +0200

    Aggiunto corso

--- app/src/main/java/com/example/progettomobilecamillonitisenigiri/Corso/CorsoActivity.kt
+++ app/src/main/java/com/example/progettomobilecamillonitisenigiri/Corso/CorsoActivity.kt
@@ -2,11 +2,25 @@ package com.example.progettomobilecamillonitisenigiri.Corso
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.progettomobilecamillonitisenigiri.R
+import com.example.progettomobilecamillonitisenigiri.databinding.ActivityCorsoBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class CorsoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_corso)
+
+        val binding: ActivityCorsoBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_corso)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.myNavHostCorsoFragment) as NavHostFragment
+
+        val navController = navHostFragment.navController
+        binding.bottomCorsoNavigation.setupWithNavController(navController)
+
     }
 }
