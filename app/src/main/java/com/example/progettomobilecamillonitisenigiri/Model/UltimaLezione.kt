b35a08cff09794d44d042ba060492cc8562e5608
Matteo Giri <s1090263@studenti.univpm.it>
2021-06-08 13:31:20 +0200

    aggiornato model utente

---
+++ app/src/main/java/com/example/progettomobilecamillonitisenigiri/Model/UltimaLezione.kt
@@ -0,0 +1,4 @@
+package com.example.progettomobilecamillonitisenigiri.Model
+
+data class UltimaLezione(val id_corso: String, val id_lezione: String, val secondi: Int)
+
--- app/src/main/java/com/example/progettomobilecamillonitisenigiri/Model/User.kt
+++ app/src/main/java/com/example/progettomobilecamillonitisenigiri/Model/User.kt
@@ -1,22 +1,37 @@
 package com.example.progettomobilecamillonitisenigiri.Model
 
-class User (uid: String, firstName: String, lastName: String, corsi: ArrayList<String>, categorie: ArrayList<String>) {
-    var uid: String
+class User (firstName: String, lastName: String, iscrizioni: ArrayList<String>, categoriePref: ArrayList<String>,wishlist: ArrayList<String>, ultimeLezioni: ArrayList<UltimaLezione>) {
     var firstName: String
     var lastName: String
-    var corsi: ArrayList<String> //id corsi
-    var categorie: ArrayList<String> //categorie
+    var iscrizioni: ArrayList<String> //lista con id dei corsi a cui l'utente è iscritto
+    var categoriePref: ArrayList<String> //categorie preferite dell'utente
+    var wishlist: ArrayList<String> //lista con id dei corsi a cui l'utente vuole iscriversi
+    var ultimeLezioni: ArrayList<UltimaLezione> //lista con le ultime lezioni viste
+
     init {
-        this.uid=uid
-        this.firstName=firstName
-        this.lastName=lastName
-        this.corsi=corsi
-        this.categorie=categorie
-        if(corsi == null)
-            this.corsi = ArrayList<String>()
-        if(categorie == null)
-            this.categorie = ArrayList<String>()
+        this.firstName = firstName
+        this.lastName = lastName
+        this.iscrizioni = iscrizioni
+        this.categoriePref = categoriePref
+        this.wishlist = wishlist
+        this.ultimeLezioni = ultimeLezioni
+        if (iscrizioni == null)
+            this.iscrizioni = ArrayList<String>()
+        if (categoriePref == null)
+            this.categoriePref = ArrayList<String>()
+        if (wishlist == null)
+            this.wishlist = ArrayList<String>()
+        if (ultimeLezioni == null)
+            this.ultimeLezioni = ArrayList<UltimaLezione>()
 
     }
-    constructor() : this("", "", "",ArrayList<String>(),ArrayList<String>()) {}
+
+    constructor() : this(
+        "",
+        "",
+        ArrayList<String>(),
+        ArrayList<String>(),
+        ArrayList<String>(),
+        ArrayList<UltimaLezione>()
+    ){}
 }
