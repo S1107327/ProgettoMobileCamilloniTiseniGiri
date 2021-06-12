1c584d85fe1f50ba7f48891e3499818df4415ee4
Andrea Camilloni <s1090325@studenti.univpm.it>
2021-06-12 09:28:52 +0200

    back button

--- app/src/main/java/com/example/progettomobilecamillonitisenigiri/Corso/CorsoActivity.kt
+++ app/src/main/java/com/example/progettomobilecamillonitisenigiri/Corso/CorsoActivity.kt
@@ -1,11 +1,15 @@
 package com.example.progettomobilecamillonitisenigiri.Corso
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.example.progettomobilecamillonitisenigiri.Main.MainActivity
 import com.example.progettomobilecamillonitisenigiri.R
 import com.example.progettomobilecamillonitisenigiri.ViewModels.FirebaseConnection
 import com.example.progettomobilecamillonitisenigiri.databinding.ActivityCorsoBinding
@@ -21,5 +25,22 @@ class CorsoActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.myNavHostCorsoFragment) as NavHostFragment
         val navController = navHostFragment.navController
         binding.bottomCorsoNavigation.setupWithNavController(navController)
+
+        setSupportActionBar(binding.topAppBarCorso)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                //val intent = Intent(this, MainActivity::class.java)
+                //startActivity(intent)
+                finish()
+            }
+        }
+        return true
     }
 }
